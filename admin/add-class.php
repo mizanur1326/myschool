013974322407207eed83be724681eb2aadb1b87f
rockRayhan <khayrulalamdict@gmail.com>
2023-09-10 15:07:15 +0600

    ads

---
+++ admin/add-class.php
@@ -0,0 +1,15 @@
+<?php include_once("../includes/index_header.php")?>
+
+<body>
+
+<?php include_once("../includes/index_sidebar.php")?>
+
+            <div class="dashboard-content-one">
+                <!-- Breadcubs Area Start Here -->
+                <div class="breadcrumbs-area">
+                    <h3>ADD CLASS</h3>
+                </div>
+                
+
+
+                <?php include_once("../includes/index_footer.php")?>
