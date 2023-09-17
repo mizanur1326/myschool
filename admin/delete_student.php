22534b2f7647dfcaf7aa0ab4f0788e1b8b9015b1
rockRayhan <khayrulalamdict@gmail.com>
2023-09-17 13:13:37 +0600

    counter animation

--- admin/delete_student.php
+++ admin/delete_student.php
@@ -1,6 +1,8 @@
 <?php 
+session_start();
 $id = $_GET["id"];
 require_once("../includes/db_config.php");
 $db->query("DELETE FROM students WHERE s_id = '$id'");
-header("Location: all_student.php");
+$_SESSION['delete_msg'] ="Data Deleted" ;
+header("Location:all_student.php");
 ?>
